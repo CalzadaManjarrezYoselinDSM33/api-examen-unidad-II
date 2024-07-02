7b7a4808ab4e58818937023d535e93a95525d412
Yoselin Calzada Manjarrez <al222110705@gmail.com>
2024-07-02 17:13:41 -0600

    Guardar

---
+++ src/main/java/com/example/demo/ApiExamenUnidadllApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiExamenUnidadllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiExamenUnidadllApplication.class, args);
+	}
+
+}
